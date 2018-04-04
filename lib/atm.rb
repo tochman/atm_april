5f5a260b09248fcee6ed4ef499a0682178a2a8c0
faraznaeem <faraznaeem@gmail.com>
2018-04-04 14:30:35 +0200

    Refactor withdraw method

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,3 +1,5 @@
+require 'Date'
+
 class ATM
 attr_accessor :funds
 
@@ -5,8 +7,23 @@ attr_accessor :funds
     @funds = 1000
   end
 
-  def withdraw(amount)
-    @funds -= amount
+  def withdraw(amount, account)
+    case
+    when insufficient_funds_in_account?(amount, account)
+      { status: false, message: 'Unsuccessful: insufficient funds in account', date: Date.today }
+    else
+      perform_transaction(amount, account)
+    end
   end
 
+  private
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def perform_transaction(amount, account)
+    @funds -= amount
+    account.balance = account.balance - amount
+    { status: true, message: 'successful', date: Date.today, amount: amount}
+  end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,16 +1,34 @@
-#As a User
- #In order to make an withdrawal
- #The ATM needs to have funds
-
 require './lib/atm.rb'
 
 describe ATM do
+
+  let(:account) {instance_double('Account')}
+   before do
+     allow(account).to receive(:balance).and_return(100)
+     allow(account).to receive(:balance=)
+   end
+
   it 'has 1000 dollars on initialize' do
     expect(subject.funds).to eq 1000
   end
 
   it 'funds are reduced on withdrawal' do
-    subject.withdraw 50
+    subject.withdraw(50, account)
     expect(subject.funds).to eq 950
+  end
+
+  it 'allow withdraw if account has enough balance' do
+    expected_output = { status: true,
+                        message: 'successful',
+                        date: Date.today,
+                        amount: 45}
+    expect(subject.withdraw(45, account)).to eq expected_output
+  end
+
+  it 'rejects withdraw if account has insufficient funds' do
+    expected_output = { status: false,
+                        message: 'Unsuccessful: insufficient funds in account',
+                        date: Date.today }
+    expect(subject.withdraw(105, account)).to eq expected_output
   end
 end
